60c2cf2d0a374dd037332c11eb455d8650bda51e
Kanad <kanadbahalkar@EIGs-MacBook-Air.local>
2016-10-16 18:00:46 -0400

    initial commit of full repository

---
+++ server/router.js
@@ -0,0 +1,53 @@
+const AuthenticationController = require('./controllers/authentication'),  
+      // UserController = require('./controllers/user'),
+      ChatController = require('./controllers/chat'),
+      express = require('express'),
+      passportService = require('./config/passport'),
+      passport = require('passport');
+
+// Middleware to require login/auth
+const requireAuth = passport.authenticate('jwt', { session: false });  
+const requireLogin = passport.authenticate('local', { session: false });
+
+// Constants for role types
+const REQUIRE_ADMIN = "Admin",  
+      REQUIRE_OWNER = "Owner",
+      REQUIRE_CLIENT = "Client",
+      REQUIRE_MEMBER = "Member";
+
+module.exports = function(app) {  
+  // Initializing route groups
+	const apiRoutes = express.Router(),
+        authRoutes = express.Router(),
+        chatRoutes = express.Router();
+
+  // Auth Routes
+  // Set auth routes as subgroup/middleware to apiRoutes
+  apiRoutes.use('/auth', authRoutes);
+
+  // Registration route
+  authRoutes.post('/register', AuthenticationController.register);
+
+  // Login route
+  authRoutes.post('/login', requireLogin, AuthenticationController.login);
+
+  // Set chat routes as a subgroup/middleware to apiRoutes
+  apiRoutes.use('/chat', chatRoutes);
+
+  // View messages to and from authenticated user
+  chatRoutes.get('/', requireAuth, ChatController.getConversations);
+
+  // Retrieve single conversation
+  chatRoutes.get('/:conversationId', requireAuth, ChatController.getConversation);
+
+  // Send reply in conversation
+  chatRoutes.post('/:conversationId', requireAuth, ChatController.sendReply);
+
+  // Start new conversation
+  chatRoutes.post('/new/:recipient', requireAuth, ChatController.newConversation);
+
+	// Set url for API group routes
+  app.use('/api', apiRoutes);
+};
+
+
