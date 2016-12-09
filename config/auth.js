18537983404e5d071b687dfc0fa4597857fb9694
Sachin Doiphode <sdoiphode@dataxu.com>
2016-12-08 23:26:20 -0500

    COnfig file

---
+++ config/auth.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '182119938859848', // your App ID
+        'clientSecret'  : '9fbdfc509d7b6d14cf5f9954855e5ee5', // your App Secret
+        'callbackURL'   : 'http://localhost:4731/auth/facebook/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '128931725263-6mof4j68lqvds8n6adjt5ft3b8vabtb3.apps.googleusercontent.com',
+        'clientSecret'  : 'npkrrCUWstpBNG7jXEJWP1v7',
+        'callbackURL'   : 'http://localhost:4731/auth/google/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : '7eKUkIImyJir2RCpWi7C1f7tG',
+        'consumerSecret'    : 'TYXKn2hMTkDQqHzllrm3V3k5vP17F07kPDSn5oO5V7FA1Awji3',
+        'callbackURL'       : 'http://localhost:4731/auth/twitter/callback'
+    }
+};
