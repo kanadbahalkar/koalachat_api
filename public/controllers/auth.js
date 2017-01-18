9f52c09a2cd183718abf3db9f74c05351da69519
Kanad Bahalkar <kanad@koalachat.com>
2017-01-18 17:29:13 -0500

    Resolving merge conflicts

--- public/controllers/auth.js
+++ public/controllers/auth.js
@@ -40,18 +40,10 @@ myApp.controller('authController', ['$route', '$routeParams', '$rootScope', '$sc
                 $log.error(res);
             } else {
                 UserService.setIsLogged(true);
-<<<<<<< HEAD
-                console.log(res)
-                // $window.localStorage.token = res.token;
-                // $window.localStorage.userid = res.user.userID;
-                // $window.localStorage.useremail = res.user.email;
-                $window.location = '/';
-=======
                 $window.localStorage.token = res.token;
                 $window.localStorage.userid = res.user.userID;
                 $window.localStorage.useremail = res.user.email;
                 $window.location = '/Overview';
->>>>>>> frontend
             }
         }, function() {
             $rootScope.error = 'Login Failed';
