a5f7c8ffee372f8f4944e2552be04a707b189cbb
Sachin Doiphode <sdoiphode@dataxu.com>
2017-01-31 22:13:32 -0500

    Done with inbox search for people

--- public/controllers/messages.js
+++ public/controllers/messages.js
@@ -7,8 +7,34 @@ myApp.controller('messagesController', ['$scope', '$location', 'VisitorsService'
 }]);
 =======
 myApp.controller('messagesController', ['$scope', '$location', function($scope, $location){
-  console.log("Inside messages controller");
+  $scope.names = [
+    {
+      'name':'Rachel Curtis',
+      'name_details': 'Nothing'
+    },
+    {
+      'name': 'John Wayne',
+      'name_details': 'Nothing'
+    },
+    {
+      'name':'Joshua Weildman',
+      'name_details':'Nothing'
+    },
+    {
+      'name':'Joshua Weildman',
+      'name_details':'Nothing'
+    },
+    {
+      'name':'Shaun Chindhi',
+      'name_details':'Nothing'
+    }
+  ]
 
-  
+  $scope.$watch('searchBox', function updateSearchText(newVal, oldVal) {
+    if (newVal !== oldVal) {
+      console.log('new Val: ', newVal);
+
+    }
+  });
 }]);
 >>>>>>> 67dcbff438532155d4da2e699652daa7b3d17ee2
